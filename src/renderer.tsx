fa2135bf64dbcf8defc185661467bbe0924d0cce
sriveer-neerukonda <sriveer.neerukonda@hotmail.com>
2023-12-10 10:15:46 +0530

    feat: init of project with react

---
+++ src/renderer.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+import App from './App';
+
+const root = document.getElementById('root') as HTMLElement;
+const reactRoot = createRoot(root);
+reactRoot.render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
+);
